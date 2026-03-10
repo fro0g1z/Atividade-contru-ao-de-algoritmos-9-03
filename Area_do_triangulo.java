5303e8cf99e056579dbe87d538f3bb78f3e71fbd
fro0g1z <pedrovipp.coelho@gmail.com>
2026-03-10 15:07:31 -0300

    Add files via upload

---
+++ Area_do_triangulo.java
@@ -0,0 +1,29 @@
+
+package area_do_triangulo;
+
+import java.util.Scanner;
+
+public class Area_do_triangulo {
+
+  
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+        
+        double base = 0;
+        double altura = 0;
+        
+        
+        System.out.println("Digite a área do triangulo com base X " );
+        base = base + leitor.nextDouble();
+        
+        System.out.println(" Digite a área do triangulo com base Y : ");
+        altura = altura + leitor.nextDouble();
+        
+        double area = (base * altura) / 2;
+        
+        System.out.println("A área do triangulo com base X e altura Y é" + base + "e altura" + altura + "e :" + area);
+                
+    
+    }
+    
+}

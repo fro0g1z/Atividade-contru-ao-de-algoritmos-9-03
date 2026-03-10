9dcf35849e686e952e7634eb6761fbb464751c50
fro0g1z <pedrovipp.coelho@gmail.com>
2026-03-10 15:55:33 -0300

    Add files via upload

---
+++ Algoritmo_Para_Calcular_a_Area_De_Um_Círculo.java
@@ -0,0 +1,29 @@
+
+package algoritmo_para_calcular_a_area_de_um_círculo;
+
+import java.util.Scanner;
+
+public class Algoritmo_Para_Calcular_a_Area_De_Um_Círculo {
+
+   
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+        
+        double raio;
+        double area;
+        double pi = 3.14;
+        
+        System.out.println("vamos calcular a area de um circulo!");
+        System.out.println("Qual o raio do circulo?");
+         raio = leitor.nextDouble();
+        
+        System.out.println("Considere pi como: 3.14");
+        pi = 3.14;
+        
+        area = pi * raio * raio;
+        
+        System.out.println(" a area do circulo com raio " + raio + " é: " + area);
+                    
+    }
+    
+}

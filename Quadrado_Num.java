a2b501cc0d2e4b0b94ce46338788a92c9f5dedaa
fro0g1z <pedrovipp.coelho@gmail.com>
2026-03-10 16:02:15 -0300

    Add files via upload

---
+++ Quadrado_Num.java
@@ -0,0 +1,13 @@
+
+package quadrado_num;
+
+public class Quadrado_Num {
+
+    public static void main(String[] args) {
+       
+        int num = 4;
+        num = num * num;
+        System.out.println(num);
+    }
+    
+}
